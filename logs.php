21fac558f19a44029594b76431a706e1b1c8c171
Guntur Dharma Putra <guntur.dharma@gmail.com>
2014-06-29 15:42:14 +0700

    Entire page completed

---
+++ logs.php
@@ -0,0 +1,134 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Logs | Rolesystem SMS</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/jumbotron-narrow.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="header">
+        <ul class="nav nav-pills pull-right">
+          <li><a href="dashboard.php">Dashboard</a></li>
+          <li><a href="users.php">Users</a></li>
+          <li class="active"><a href="logs.php">Logs</a></li>
+          <li><a href="send-sms.php">Send SMS</a></li>
+        </ul>
+        <h3 class="text-muted">Rolesystem SMS</h3>
+      </div>
+
+      <div class="content">
+        <h1>Communication Logs <small>+62 815 7876 2345</small></h1>
+
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Datetime  <small><span class="glyphicon glyphicon-sort"></span></small></th>
+              <th>Message Content</th>
+              <th>Type</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>2013-12-29 19:49:50</td>
+              <td>TIDAK</td>
+              <td>
+                <button type="button" class="btn btn-info btn-xs">
+                  <span class="glyphicon glyphicon-arrow-down"></span>
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-default btn-xs">
+                  <span class="glyphicon glyphicon-search"></span> See details
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>2013-12-29 19:49:11</td>
+              <td>Apakah terdapat titik-titik merah di kulit?</td>
+              <td>
+                <button type="button" class="btn btn-success btn-xs">
+                  <span class="glyphicon glyphicon-arrow-up"></span>
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-default btn-xs">
+                  <span class="glyphicon glyphicon-search"></span> See details
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>2013-12-29 19:48:11</td>
+              <td>YA</td>
+              <td>
+                <button type="button" class="btn btn-info btn-xs">
+                  <span class="glyphicon glyphicon-arrow-down"></span>
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-default btn-xs">
+                  <span class="glyphicon glyphicon-search"></span> See details
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td>2013-12-29 19:47:11</td>
+              <td>Terimakasih, data anda sudah tersimpan. Silakan balas 'YA' untuk memulai konsultasi.</td>
+              <td>
+                <button type="button" class="btn btn-success btn-xs"><span class="glyphicon glyphicon-arrow-up"></span></button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-default btn-xs">
+                  <span class="glyphicon glyphicon-search"></span> See details
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>5</td>
+              <td>2013-12-29 19:46:11</td>
+              <td>REG Mark</td>
+              <td>
+                <button type="button" class="btn btn-info btn-xs"><span class="glyphicon glyphicon-arrow-down"></span></button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-default btn-xs">
+                  <span class="glyphicon glyphicon-search"></span> See details
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="footer">
+        <p>&copy; Rolesystem 2014</p>
+      </div>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
+</html>
---
+++ users.php
@@ -0,0 +1,102 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Users | Rolesystem SMS</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/jumbotron-narrow.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="header">
+        <ul class="nav nav-pills pull-right">
+          <li><a href="dashboard.php">Dashboard</a></li>
+          <li class="active"><a href="users.php">Users</a></li>
+          <li><a href="logs.php">Logs</a></li>
+          <li><a href="send-sms.php">Send SMS</a></li>
+        </ul>
+        <h3 class="text-muted">Rolesystem SMS</h3>
+      </div>
+
+      <div class="content">
+        <h1>Registered Numbers</h1>
+
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Phone Number</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>+62 815 7876 2345</td>
+              <td>
+                <button type="button" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-search"></span> See logs</button>
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>John</td>
+              <td>+62 815 7876 2345</td>
+              <td>
+                <button type="button" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-search"></span> See logs</button>
+              </td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Robert</td>
+              <td>+62 815 7876 2345</td>
+              <td>
+                <button type="button" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-search"></span> See logs</button>
+              </td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td>Robin</td>
+              <td>+62 815 7876 2345</td>
+              <td>
+                <button type="button" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-search"></span> See logs</button>
+              </td>
+            </tr>
+            <tr>
+              <td>5</td>
+              <td>Van Gaal</td>
+              <td>+62 815 7876 2345</td>
+              <td>
+                <button type="button" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-search"></span> See logs</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="footer">
+        <p>&copy; Rolesystem 2014</p>
+      </div>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
+</html>
