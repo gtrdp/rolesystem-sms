ded3a5f8bb749c484bd37ec1d95f8401d851d80b
Guntur Dharma Putra <guntur.dharma@gmail.com>
2014-06-29 14:19:10 +0700

    Adding bootstrap assets

---
+++ main.php
@@ -0,0 +1,14 @@
+<?php
+
+// check unprocessed inbox
+
+// for each number in unprocessed inbox
+
+// 		check the current stage of the number
+//		whether it is case selection, ident gejala, or ident gejala2
+
+//		if complete, set the number stage to finish stage
+
+
+
+?>
