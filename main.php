f4443dee70a517fad5592aaa9299101efec4510f
Guntur Dharma Putra <guntur.dharma@gmail.com>
2014-06-30 23:06:19 +0700

    Small work in monday night

--- main.php
+++ main.php
@@ -1,6 +1,23 @@
 <?php
 
+// for gammu DB
+$gammu_db = new mysqli("localhost", "root", "root", "smsd");
+if ($gammu_db->connect_errno) {
+    echo "Failed to connect to MySQL: " . $gammu_db->connect_error;
+}
+
+// for rolesystem DB
+$rolesystem_db = new mysqli("localhost", "root", "root", "rolesystem");
+if ($rolesystem_db->connect_errno) {
+    echo "Failed to connect to MySQL: " . $rolesystem_db->connect_error;
+}
+
 // check unprocessed inbox
+$result = $gammu_db->query("SELECT * FROM inbox WHERE Processed == 'false'");
+
+while ($row = $result->fetch_assoc()) {
+	$result2 = $gammu_db->query("SELECT * FROM reg WHERE phone_number = ");
+}
 
 // for each number in unprocessed inbox
 
