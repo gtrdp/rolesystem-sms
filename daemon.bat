8aed4773f6e5c6cd34a1535cd847e769b2bb658e
gtrdp <guntur.dharma@gmail.com>
2014-08-19 15:37:27 +0700

    Processor OK

---
+++ daemon.bat
@@ -0,0 +1,10 @@
+
+
+:: Configure this (use absolute path)
+:: php cli path
+set PHP=C:\xampp\php\php.exe
+:: daemon.php path
+set DAEMON=C:\xampp\htdocs\rolesystem-sms\processor.php
+
+:: Execute
+%PHP% %DAEMON%
