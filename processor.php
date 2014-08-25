66c422c1b1bc24317ebd3e7fccacd7e1e7ac75ee
Guntur Dharma Putra <guntur.dharma@gmail.com>
2014-08-25 11:17:48 +0700

    Hasil coding di komputer pak is

--- processor.php
+++ processor.php
@@ -13,15 +13,55 @@
  *  - kasusID;role_identnext;answer_identnext;ke: consultation process
  */
 $mysqli = new mysqli('localhost', 'root', '', 'gammu');
-// $rolesystem = new mysqli('localhost', 'root', '', 'rolesystem');
 
 if($mysqli->connect_errno)
 	exit($mysqli->connect_error);
-// if($rolesystem->connect_errno)
-// 	exit($rolesystem->connect_error);
 
+// get the message string and phone number
+$query = <<<SQL
+	SELECT *
+	FROM inbox
+	WHERE Processed = 'false'
+SQL;
+$raw_data = $mysqli->query($query);
+
+$data = $raw_data->fetch_object();
+
+$sender = $data->SenderNumber;
+$date = $data->ReceivingDateTime;
+$message = $data->TextDecoded;
+
+if(substr($message, 0, 3) == 'REG'){
+	$nama = trim(substr($message, 3));
+	$query = "INSERT INTO outbox (DestinationNumber, TextDecoded, CreatorID) ".
+						 "VALUES ('$sender', 'Terimakasih. Anda telah terdaftar Sdr. $nama . Balas mulai untuk mulai konsultasi.', 'Gammu')";
+	$mysqli->query($query);
+	$query = "UPDATE inbox SET Processed = 'true' WHERE ReceivingDateTime = '$date'";
+	$mysqli->query($query);
+}else if($message == 'mulai'){
+	$query = "INSERT INTO outbox (DestinationNumber, TextDecoded, CreatorID) ".
+						 "VALUES ('$sender', 'Apakah ada bintik-bintik merah di kulit? Ya (Y). Tidak (T).', 'Gammu')";
+	$mysqli->query($query);
+	$query = "UPDATE inbox SET Processed = 'true' WHERE ReceivingDateTime = '$date'";
+	$mysqli->query($query);
+}else if($message == 'Y'){
 $query = "INSERT INTO outbox (DestinationNumber, TextDecoded, CreatorID) ".
-					 "VALUES ('081578762345', 'Terimakasih. Anda telah terdaftar.', 'Gammu')";
-$mysqli->query($query);
+						 "VALUES ('$sender', 'Diagnosis: Demam Berdarah. Saran: Segera ke dokter.', 'Gammu')";
+	$mysqli->query($query);
+	$query = "UPDATE inbox SET Processed = 'true' WHERE ReceivingDateTime = '$date'";
+	$mysqli->query($query);
+}else if($message == 'T'){
+	$query = "INSERT INTO outbox (DestinationNumber, TextDecoded, CreatorID) ".
+						 "VALUES ('$sender', 'Diagnosis: Insya Allah Sehat. Saran: Jaga kesehatan.', 'Gammu')";
+	$mysqli->query($query);
+	$query = "UPDATE inbox SET Processed = 'true' WHERE ReceivingDateTime = '$date'";
+	$mysqli->query($query);
+}else{
+	$query = "INSERT INTO outbox (DestinationNumber, TextDecoded, CreatorID) ".
+						 "VALUES ('$sender', 'Maaf format anda salah. Balas mulai untuk mulai konsultasi.', 'Gammu')";
+	$mysqli->query($query);
+	$query = "UPDATE inbox SET Processed = 'true' WHERE ReceivingDateTime = '$date'";
+	$mysqli->query($query);
+}
 
 ?>
